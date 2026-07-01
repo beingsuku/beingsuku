f4a183b9f2dae9d5a968d7d7d4931b57edf1be28
beingsuku <sakshiredekar452@gmail.com>
2026-07-01 19:55:31 +0530

    Initial commit

---
+++ to-do-list.py
@@ -0,0 +1,67 @@
+tasks=[]
+import json
+
+def save_tasks():
+       with open("tasks.json", "w") as f:
+          json.dump(tasks, f)
+
+def load_tasks():
+       global tasks
+       try:
+           with open("tasks.json", "r") as f:
+              tasks = json.load(f)
+       except FileNotFoundError:
+          tasks = []
+def add_task():
+    task= input("Enter a task bae the realistic one!:")
+    tasks.append(task)
+    save_tasks()
+
+def view_tasks():
+    if not tasks:
+        print("Ahh u forget to add baby just like u forget him! ha ha..")
+        return 
+    for i in range(len(tasks)):
+        print(i+1,".",tasks[i])
+
+def remove_task():
+    if not tasks:
+        print("Ahh u forget to add baby just like u forget him! ha ha..")
+        return 
+    view_tasks()
+    try:
+        to_remove=int(input("Enter which task you wanna remove:"))
+        removed = tasks.pop(to_remove-1)
+        print(f"Removed as you said Majesty: {removed}")
+        save_tasks()
+    except (ValueError,IndexError):
+        print("That's not a valid task,Enter a valid one gurlll!")
+
+def main():
+    load_tasks()
+    while True:
+          print("1.Add task\n2.View\n3.Remove\n4.Exit")
+          try:
+            choice= int(input("Choose:"))
+          except ValueError:
+              print("Please enter a number babe!")
+              continue
+          match choice:
+            case 1:
+                add_task()
+            case 2:
+                view_tasks()
+            case 3:
+                remove_task()
+            case 4 :
+                print("Successfully executed!")
+                break
+            case _ :
+                print("Invalid input!!")
+
+main()
+                
+            
+
+
+
