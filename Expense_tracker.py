112a03e47d3905a8a50c0aca33aacebe80c0ddb5
beingsuku <sakshiredekar452@gmail.com>
2026-07-18 00:59:46 +0530

    Created expense tracker

--- Expense_tracker.py
+++ Expense_tracker.py
@@ -1,72 +1,83 @@
-expense=[]
-
 from datetime import date
+expenses=[]
 import csv
 
 def save_expenses():
     with open("expenses.csv", "w", newline="") as f:
         writer = csv.DictWriter(f, fieldnames=["amount", "category", "date"])
         writer.writeheader()
-        writer.writerows(expense)
+        writer.writerows(expenses)
 
 def load_expenses():
-    global expense
+    global expenses
     try:
         with open("expenses.csv", "r") as f:
             reader = csv.DictReader(f)
-            expense = list(reader)
+            expenses = list(reader)
     except FileNotFoundError:
-        expense = []
-
-def add_expense():
-    try:
-        amount=float(input("Enter the amount:"))
-    except ValueError:
-        print("Enter a valid number!")
-        return
-    category = input("Enter the category:")
-    today = date.today()
-
-    entry = {f"Amount":amount,"category":category,"date":today}
-    expense.append(entry)
-
-def view_expenses():
-    if not expense :
-        print("This is empty,u have not spent yet!")
-    for x in expense :
-        print( x )
+        expenses = []
 
 def total_spending():
-    for x in expense:
-        total=sum(item["amount"] for item in expense)
+        total=sum(float(x['amount']) for x in expenses)
         print(total)
 
 def category_breakdown():
-     pass
+    breakdown = {}
+    for x in expenses:
+        category = x["category"]
+        amount = float(x["amount"])
+        if category not in breakdown:
+            breakdown[category] = 0
+        breakdown[category] += amount
+    
+    for category, total in breakdown.items():
+        print(f"{category}: {total}")
+def add_expense():
+    try :
+        amount=float(input("Enter the amount:"))
+    except ValueError:
+        print("Enter valid Number")
+        return
+    category=input("Enter category:")
+    today = date.today()
+
+    entry = {"amount":amount,"category":category,"date":today}
+
+    expenses.append(entry)
+    print("Expense added!")
+    save_expenses()
+
+def view_expense():
+    if not expenses:
+        print("You haven't spent yet")
+        return 
+    for x in expenses:
+         print(f"Amount: {x['amount']} | Category: {x['category']} | Date: {x['date']}")
 
 def main():
     load_expenses()
     while True:
-        print("1.Add_expense 2.View_expense 3.Total_spending 4.Breakdown 5.Exit")
+        print("1.Add expense 2.View expenses 3.View total spending 4.breakdown 5.Exit")
         try:
-            choice = int(input("Enter a choice!"))
-        except ValueError:
-            print("Enter valid option!")
+            choice=int(input("Enter your choice:"))
+        except(ValueError,IndexError):
+            print("Enter Valid one!")
             continue
         match choice :
             case 1:
                 add_expense()
-                save_expenses()
             case 2:
-                view_expenses()
+                view_expense()
             case 3:
                 total_spending()
             case 4:
                 category_breakdown()
             case 5:
-                print("Saved changes!")
+                print("See you after new spending!")
                 break
-            case _:
-                print("Enter a valid choice")
+            case _ :
+                print("Enter valid choice!")
 
 main()
+
+         
